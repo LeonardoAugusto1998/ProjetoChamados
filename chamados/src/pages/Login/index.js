118cb56e21ce3d9cb51c69974e28039212fdfcca
Leonardo Augusto <Leonardo1998.augusto@gmail.com>
2021-05-25 18:02:43 -0300

    commit 25/05 18:02

--- chamados/src/pages/Login/index.js
+++ chamados/src/pages/Login/index.js
@@ -1,14 +1,35 @@
 
+import './login.css'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 export default function Login(){
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
 
-   
+    function acessar(e){
+        e.preventDefault();
+    }
+
     return(
-    
-        <div>
-            <h1>Página de Login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='logo-area'>
+                    <img src={logo} alt='logo'/>
+                </div>
+
+                <form onSubmit={acessar}>
+                    <h1>Entrar</h1>
+                    <label>Login</label>
+                    <input type='email' value={email} placeholder='exemplo@exemplo.com' onChange={(e)=>{setEmail(e.target.value)}}/>
+                    <label>Senha</label>
+                    <input type='password' value={senha} placeholder='********' onChange={(e)=>{setSenha(e.target.value)}}/>
+                    <button type='submit'>Acessar</button>
+                    <Link to='/cadastro'>Criar uma Conta</Link>
+                </form>
+            </div>
         </div>
-      
     )
 }
 
