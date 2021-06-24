237ba5cf7243d571cdf3bed43fbaba424dcf020f
Leonardo Augusto <Leonardo1998.augusto@gmail.com>
2021-06-24 13:16:33 -0300

    commit de mais um component o Header

--- chamados/src/components/Header/index.js
+++ chamados/src/components/Header/index.js
@@ -5,16 +5,22 @@ import { AuthContext } from '../../contexts/auth';
 import { Link } from 'react-router-dom';
 import avatar from '../../assets/avatar.png';
 import cover from '../../assets/cover.png';
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
 
 export default function Header(){
 
 const { user } = useContext(AuthContext);
 
     return(
-        <div>
-            <img src={user.avatarUrl == null ? avatar : user.avatarUrl} alt='foto de perfil'/>
-            <img src={cover} alt='cover'/>
+        <div className='sidebar'>
+            <div>
+                <img src={user.avatarUrl === null ? avatar : user.avatarUrl} alt='foto de perfil'/>
+            </div>
             
-        </div>qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+            <Link to='/dashboard'>Clientes <FiHome size={24}/></Link> 
+            <Link to='/dashboard'>Chamados <FiSettings size={24}/></Link>
+            <Link to='/dashboard'>Meu Perfil <FiUser size={24}/></Link>
+            
+        </div>
     );
 }
